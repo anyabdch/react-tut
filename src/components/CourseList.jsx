3b6d5824558db0dfee9bd7cb276281538fc64fa3
anyabdch <anya.bardach@gmail.com>
2024-10-08 16:27:47 -0500

    add styling

--- src/components/CourseList.jsx
+++ src/components/CourseList.jsx
@@ -1,9 +1,18 @@
+import './CourseList.css'
+
 const Course = ({course}) => (
-    <p>{course.term} CS{course.number}: {course.title}</p>
+    <div className="card m-1 p-2">
+        <div className="card-body">
+            <h5 className="card-title">{course.term} CS{course.number}</h5>
+            <p className="card-text">{course.title}</p>
+        </div>
+        <hr></hr>
+        <p className="card-text">{course.meets}</p>
+    </div>
 );
 
 const CourseList = ({courses}) => (
-    <div>
+    <div className='course-list'>
         { Object.entries(courses).map(([id, cur]) => <Course course={cur}/>)}
     </div>
 );
